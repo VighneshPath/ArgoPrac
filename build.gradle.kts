958104fc0f34d2c779b742714102039bea21fb60
VighneshPath <pathrikarvighnesh@gmail.com>
2023-07-28 15:33:15 +0530

    Updating version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("com.palantir.docker") version "0.35.0"
 }
 
-version = "0.1.3"
+version = "0.1.4"
 group = "example.micronaut"
 
 val kotlinVersion=project.properties.get("kotlinVersion")
